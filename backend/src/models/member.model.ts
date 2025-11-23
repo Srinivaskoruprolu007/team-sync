1b752c9541881d083cd2c2400d5f86789064875a
srinivas <srinivaskoruprolu5@gmail.com>
2025-11-23 14:22:39 +0530

    feat: update Member and Role models to use ObjectId for role references and enhance schema structure

--- backend/src/models/member.model.ts
+++ backend/src/models/member.model.ts
@@ -3,32 +3,36 @@ import mongoose, { Document, Schema } from "mongoose";
 export interface MemberDocument extends Document {
     userId: mongoose.Types.ObjectId;
     workspace: mongoose.Types.ObjectId;
-    // role: string; //TODO create Role Document
+    role: mongoose.Types.ObjectId;
     joinedAt: Date;
 }
 
-const MemberSchema = new Schema<MemberDocument>({
-    userId: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
+const MemberSchema = new Schema<MemberDocument>(
+    {
+        userId: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        workspace: {
+            type: Schema.Types.ObjectId,
+            ref: "Workspace",
+            required: true,
+        },
+        role: {
+            type: Schema.Types.ObjectId,
+            ref: "Role",
+            required: true,
+        },
+        joinedAt: {
+            type: Date,
+            required: true,
+        },
     },
-    workspace: {
-        type: Schema.Types.ObjectId,
-        ref: "Workspace",
-        required: true,
-    },
-    // role: {
-    //     type: String,
-    //     required: true,
-    // },
-    joinedAt: {
-        type: Date,
-        required: true,
-    },
-}, {
-    timestamps: true,
-});
+    {
+        timestamps: true,
+    }
+);
 
 const MemberModel = mongoose.model<MemberDocument>("Member", MemberSchema);
 export default MemberModel;
