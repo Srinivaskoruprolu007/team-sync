87b6911076365894338402b9a91da6f535e15b6b
srinivas <srinivaskoruprolu5@gmail.com>
2025-11-23 14:15:24 +0530

    feat: update role permissions and add OWNER role with specific permissions

--- backend/src/utils/roles-permission.ts
+++ backend/src/utils/roles-permission.ts
@@ -3,11 +3,6 @@ import { Permissions, PermissionType, RoleType } from "@/enums/role.enum";
 export const RolePermssions: Record<RoleType, Array<PermissionType>> = {
     ADMIN: [
         Permissions.ADD_MEMBER,
-        Permissions.REMOVE_MEMBER,
-        Permissions.CHANGE_MEMBER_ROLE,
-        Permissions.CREATE_WORKSPACE,
-        Permissions.DELETE_WORKSPACE,
-        Permissions.EDIT_WORKSPACE,
         Permissions.MANAGE_WORKSPACE_SETTINGS,
         Permissions.CREATE_PROJECT,
         Permissions.DELETE_PROJECT,
@@ -15,28 +10,31 @@ export const RolePermssions: Record<RoleType, Array<PermissionType>> = {
         Permissions.CREATE_TASK,
         Permissions.DELETE_TASK,
         Permissions.EDIT_TASK,
-        Permissions.ADD_MEMBER,
-    ],
-    USER: [
-        Permissions.ADD_MEMBER,
-        Permissions.REMOVE_MEMBER,
-        Permissions.CHANGE_MEMBER_ROLE,
-        Permissions.CREATE_PROJECT,
-        Permissions.DELETE_PROJECT,
-        Permissions.EDIT_PROJECT,
-        Permissions.CREATE_TASK,
-        Permissions.DELETE_TASK,
-        Permissions.EDIT_TASK,
+        Permissions.VIEW_ONLY,
     ],
     MEMBER: [
-        Permissions.ADD_MEMBER,
-        Permissions.REMOVE_MEMBER,
-        Permissions.CHANGE_MEMBER_ROLE,
+        Permissions.VIEW_ONLY,
+        Permissions.CREATE_TASK,
+        Permissions.EDIT_TASK,
+    ],
+    OWNER: [
+        Permissions.CREATE_WORKSPACE,
+        Permissions.DELETE_WORKSPACE,
+        Permissions.EDIT_WORKSPACE,
+        Permissions.MANAGE_WORKSPACE_SETTINGS,
+
         Permissions.CREATE_PROJECT,
-        Permissions.DELETE_PROJECT,
         Permissions.EDIT_PROJECT,
+        Permissions.DELETE_PROJECT,
+
         Permissions.CREATE_TASK,
         Permissions.DELETE_TASK,
         Permissions.EDIT_TASK,
+
+        Permissions.ADD_MEMBER,
+        Permissions.REMOVE_MEMBER,
+        Permissions.CHANGE_MEMBER_ROLE,
+
+        Permissions.VIEW_ONLY,
     ],
 };
