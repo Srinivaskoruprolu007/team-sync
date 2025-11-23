592e05d12e2702611069e5ab8f78ac31d6c663d6
srinivas <srinivaskoruprolu5@gmail.com>
2025-11-23 14:22:39 +0530

    feat: update Member and Role models to use ObjectId for role references and enhance schema structure

--- backend/src/models/role.model.ts
+++ backend/src/models/role.model.ts
@@ -1,7 +1,33 @@
-import { PermissionType, RoleType } from '@/enums/role.enum';
-import { Document } from 'mongoose';
+import { PermissionType, RoleEnum, RoleType } from "@/enums/role.enum";
+import { RolePermssions } from "@/utils/roles-permission";
+import mongoose, { Document, Schema } from "mongoose";
 
-export interface RoleDocument extends Document{
-    name:RoleType;
-    permissions:Array<PermissionType>;
+export interface RoleDocument extends Document {
+    name: RoleType;
+    permissions: Array<PermissionType>;
 }
+
+const RoleSchema = new Schema<RoleDocument>(
+    {
+        name: {
+            type: String,
+            enum: Object.values(RoleEnum),
+            required: true,
+            unique: true,
+        },
+        permissions: {
+            type: [String],
+            enum: Object.values(Permissions),
+            required: true,
+            default: function (this: RoleDocument) {
+                return RolePermssions[this.name];
+            },
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const RoleModel = mongoose.model<RoleDocument>("Role", RoleSchema);
+export default RoleModel;
